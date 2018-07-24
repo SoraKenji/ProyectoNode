537fdd318e5d8c15820831eb18ae2ea07047ba7d
cltorres <ctorres@qualitatcorp.cl>
2018-07-24 17:21:31 -0400

    asdf_2

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
-module.exports = {
-    database:{
+module.exports = function () {
+    var con = mysql.createConnection({
         host: "localhost",    // ip address of server running mysql
         user: "satodb",    // user name to your mysql database
-        password: "teamoruben"    // corresponding password
-    }
+        password: "teamoruben",    // corresponding password
+    });
+
+    return con;
   }
--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const port = 1102;
 var mysql = require('mysql');
  
 // create a connection variable with the details required
-var con = mysql.createConnection({
-  host: "localhost",    // ip address of server running mysql
-  user: "satodb",    // user name to your mysql database
-  password: "teamoruben",    // corresponding password
-});
+con = require('./config/db')
  
 app.use(bodyParser.urlencoded({ extended: true }));
 // connect to the database.

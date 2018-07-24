a66764a6a0b041af74f28c02c6a3f31709fcc7fc
cltorres <ctorres@qualitatcorp.cl>
2018-07-24 17:16:02 -0400

    asdf

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database:{
+        host: "localhost",    // ip address of server running mysql
+        user: "satodb",    // user name to your mysql database
+        password: "teamoruben"    // corresponding password
+    }
+  }
--- server.js
+++ server.js
@@ -5,10 +5,29 @@ const app            = express();
 
 const port = 1102;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+///
 
-require('./app/routes')(app,{});
-
-app.listen(port, ()=> {
-    console.log('Corriendo en el puerto ' + port);
+// include mysql module
+var mysql = require('mysql');
+ 
+// create a connection variable with the details required
+var con = mysql.createConnection({
+  host: "localhost",    // ip address of server running mysql
+  user: "satodb",    // user name to your mysql database
+  password: "teamoruben",    // corresponding password
 });
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+// connect to the database.
+con.connect(function(err) {
+    if (err) throw err;
+    require('./app/routes')(app, {});
+    app.listen(port, ()=> {
+        console.log('Corriendo en el puerto ' + port);
+        console.log("Connected!");
+    });
+});
+
+///
+
+
